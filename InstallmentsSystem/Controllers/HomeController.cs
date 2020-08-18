a1e672c890d4a30ed4a5971bbdbcc83d7a59c75f
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-18 14:25:08 +0200

    Update namespace

--- InstallmentsSystem/Controllers/HomeController.cs
+++ InstallmentsSystem/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using InstallmentsSystem.Entities.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using InstallmentsSystem.Models;
 
 namespace InstallmentsSystem.Controllers
 {
