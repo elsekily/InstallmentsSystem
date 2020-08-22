170375debf6403c7d3e1b67627584d97321694b1
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-22 19:59:18 +0200

    Add Identity

---
+++ InstallmentsSystem/Entities/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace InstallmentsSystem.Entities.Models
+{
+    public class Role : IdentityRole<int>
+    {
+        public ICollection<UserRole> User { get; set; }
+
+    }
+}
--- InstallmentsSystem/Persistence/InstallmentsSystemDbContext.cs
+++ InstallmentsSystem/Persistence/InstallmentsSystemDbContext.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using InstallmentsSystem.Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace InstallmentsSystem.Persistence
 {
-    public class InstallmentsSystemDbContext : DbContext
+    public class InstallmentsSystemDbContext : IdentityDbContext<User, Role, int, IdentityUserClaim<int>,
+        UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Installment> installments { get; set; }
+        public DbSet<Payment> Payments { get; set; }
         public InstallmentsSystemDbContext(DbContextOptions<InstallmentsSystemDbContext> options)
         : base(options)
         {
