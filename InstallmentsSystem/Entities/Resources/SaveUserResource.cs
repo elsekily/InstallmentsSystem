4f2b3a76923b109c7a3ed8de44556c111b00fe58
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-23 15:18:17 +0200

    add Account Controller

---
+++ InstallmentsSystem/Entities/Resources/SaveUserResource.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InstallmentsSystem.Entities.Resources
+{
+    public class SaveUserResource
+    {
+        [Required]
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
