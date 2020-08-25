7d55e267ecd0a4f20ec79dcb9ad2b50fb12bd923
Youssef Elsekily <youssefelsekily@outlook.com>
2020-08-25 17:50:02 +0200

    updating intial views

--- InstallmentsSystem/Controllers/HomeController.cs
+++ InstallmentsSystem/Controllers/HomeController.cs
@@ -16,14 +16,9 @@ namespace InstallmentsSystem.Controllers
 
         public IActionResult Index()
         {
+
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
